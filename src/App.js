0766edc5b8ff5b40f8499d2b4e991f8017998b36
KALalev18 <KALalev18@codingburgas.bg>
2024-08-23 20:48:42 +0300

    implement simple design, add background and border colors in the qr code

--- src/App.js
+++ src/App.js
@@ -1,29 +1,55 @@
 import React, { useState } from "react";
 import QRCode from "qrcode.react";
+import "./App.css"
 
 export default function QRCodeGenerator() {
     const [qrCode, setQrCode] = useState("");
     const [input, setInput] = useState("");
+    const [backgroundColor, setBackgroundColor] = useState("#ffffff")
+    const [showQrCode, setShowQrCode] = useState(false);
+
     function handleGenerateQrCode() {
         setQrCode(input);
+        setShowQrCode(true);
         //setInput(""); - when you want to reset it on pressed button
     }
-    return (<div>
-            <h1>QR Code Generator</h1>
-            <div>
+    return (<div className="container">
+        <h1>QR Code Generator</h1>
+        <div>
+            <input
+                onChange={(e) => setInput(e.target.value)}
+                type="text"
+                name="qr-code"
+                value={input}
+                placeholder="Enter your value" />
+
+            <button
+                disabled={input && input.trim() !== "" ? false : true}
+                onClick={handleGenerateQrCode}>Generate</button>
+        </div>
+        <div>
+            {qrCode && (
+                <div className={`qr-code-container ${showQrCode ? "show" : ""}`}>
+                    <QRCode
+                        id="qr-code-value"
+                        value={qrCode || input}
+                        size={600}
+                        bgColor={backgroundColor} />
+
+
+                </div>
+            )}
+            <div className="color-picker-container">
+                <label htmlFor="color-picker"><h2>Choose background color:</h2></label>
                 <input
-                    onChange={(e) => setInput(e.target.value)}
-                    type="text"
-                    name="qr-code"
-                    value={input}
-                    placeholder="Enter your value"/>
-                <button
-                    disabled={input && input.trim() !== "" ? false : true}
-                    onClick={handleGenerateQrCode}>Generate</button>
-            </div>
-            <div>
-                {qrCode && <QRCode id="qr-code-value" value={qrCode} size={400} bgColor="#fff" />}
+                    id="color-picker"
+                    type="color"
+                    onChange={(e) => setBackgroundColor(e.target.value)}
+                    value={backgroundColor}
+                    className="color-picker"
+                />
             </div>
         </div>
+    </div>
     );
 }
